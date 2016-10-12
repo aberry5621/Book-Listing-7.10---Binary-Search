9a1a97e6525e25fcd3a58815c90ff7824a23cead
alexberry721 <alexberry721@gmail.com>
2016-10-12 13:38:00 -0700

    Initial Commit

---
+++ Book Listing 7.10 - Binary Search/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Book Listing 7.10 - Binary Search
+//
+//  Created by ax on 10/12/16.
+//  Copyright © 2016 COMP130. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
