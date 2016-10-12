205ff3180f38680d9c8792ed77afb1b69da9681c
alexberry721 <alexberry721@gmail.com>
2016-10-12 13:47:38 -0700

    Binary search of array values

--- Book Listing 7.10 - Binary Search/main.cpp
+++ Book Listing 7.10 - Binary Search/main.cpp
@@ -7,9 +7,31 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int binary_search(const int list[], int key, int list_size) {
+    int low = 0;
+    int high = list_size - 1;
+    
+    while(low <= high) {
+        int mid = (low + high) / 2;
+        if (key < list[mid]) {
+            high = mid -1;
+        } else if (key == list[mid]) {
+            return mid;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    
+    return -1;
+}
+
+
+int main() {
+    
+    // call binary_search
+    
     return 0;
 }
